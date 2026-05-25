8e55ab8b354c4b8cfba86ad5c1623831b09103d2
HuyCanXaK7 <huycanxak7@gmail.com>
2026-05-25 20:21:54 +0700

    Create BT3.py

---
+++ SESSION 01/BT3/BT3.py
@@ -0,0 +1,9 @@
+print(' --- HỆ THỐNG TIẾP NHẬN BỆNH NHÂN --- ')
+name_patient = input('Nhập tên bệnh nhân: ')
+code_ba = (input('Mã BA: '))
+phongkham = input('chuyển tới khoa: ')
+
+print(' --- PHIẾU KHÁM BỆNH --- ')
+print('Tên bệnh nhân:', name_patient)
+print('Mã BA:', code_ba)
+print('Khoa:', phongkham)
